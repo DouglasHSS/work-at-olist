cfc3e5ceb99bde3a33b2349ebee56ab734ad6248
Douglas Henrique Santana da silva <douglas_hss@hotmail.com.br>
2017-09-24 22:03:02 -0300

    - Creating Channel and Category models and their very first tests.

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,5 +1,44 @@
 # -*- coding: utf-8 -*-
+import uuid
 
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class UuidPkModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Channel(UuidPkModel):
+    name = models.CharField(max_length=100, blank=False)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Channel"
+        verbose_name_plural = "Channels"
+
+    def __str__(self):
+        return self.name
+
+
+class Category(UuidPkModel):
+
+    parent_category = models.ForeignKey("Category",
+                                        related_name="subcategories",
+                                        verbose_name="Channel", null=True)
+    channel = models.ForeignKey("Channel", related_name="categories",
+                                verbose_name="Channel")
+    name = models.CharField(max_length=20, blank=False, verbose_name="Name")
+
+    class Meta:
+        ordering = ["channel__name", "name"]
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return "{channel_name} - {category_name}".format(
+            channel_name=self.channel.name,
+            category_name=self.name
+        )
--- apps/core/tests/test_models.py
+++ apps/core/tests/test_models.py
@@ -1,5 +1,31 @@
 # -*- coding: utf-8 -*-
 
-# from django.test import TestCase
+from django.test import TestCase
+from model_mommy import mommy
 
-# Create your tests here.
+
+class ChannelTest(TestCase):
+
+    def test_str_method(self):
+        channel = mommy.make("Channel")
+        self.assertEqual(str(channel), channel.name)
+
+    def test_create_channel(self):
+        mommy.make("Channel")
+
+
+class CategoryTest(TestCase):
+
+    def test_str_method(self):
+        channel = mommy.make("Channel")
+        category = mommy.make("Category", channel=channel)
+
+        str_ = "{channel_name} - {category_name}".format(
+            channel_name=category.channel.name,
+            category_name=category.name
+        )
+
+        self.assertEqual(str(category), str_)
+
+    def test_create_category(self):
+        mommy.make("Category")
