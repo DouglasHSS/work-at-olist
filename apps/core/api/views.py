c5512cf0301ec0f13f01121168bc62906185f8e6
Douglas Henrique Santana da silva <douglas_hss@hotmail.com.br>
2017-09-26 16:16:28 -0300

    - Changing ChannelViewSet and CategoryViewSet to switch serializers according to ViewSet action.

--- apps/core/api/views.py
+++ apps/core/api/views.py
@@ -2,15 +2,24 @@
 
 from rest_framework import viewsets
 
-from apps.core.api.serializer import ChannelSerializer, CategorySerializer
+from apps.core.api.serializer import ChannelListSerializer, ChannelDetailSerializer,\
+    CategoryListSerializer, CategoryDetailSerializer
 from apps.core.models import Channel, Category
 
 
 class ChannelViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Channel.objects.all()
-    serializer_class = ChannelSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return ChannelDetailSerializer
+        return ChannelListSerializer
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return CategoryDetailSerializer
+        return CategoryListSerializer
